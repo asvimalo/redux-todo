3ce9adc92f282fe1227ced356280a1a635729d44
circoloco <asvimalo@gmail.com>
2017-04-01 19:50:16 +0200

    add support for location fetching

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,4 +1,5 @@
-var redux = require('redux');
+const redux = require('redux');
+const axios = require('axios');
 
 console.log('Starting redux example');
 // argument needs to be a pure function = reducer takes existing state and actions as arguments and it computes the new state
@@ -89,10 +90,53 @@ var removeMovie = (id) => {
   }
 }
 
+// Map reducer and action generators
+// ----------------------------------
+var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+  switch(action.type) {
+    case 'START_LOCATION_FETCH':
+      return {
+        isFetching: true,
+        url: undefined
+      }
+    case 'COMPLETE_LOCATION_FETCH':
+      return {
+        isFetching: false,
+        url: action.url
+      }
+    default:
+      return state;
+  }
+};
+
+var startLocationFetch = () => {
+  return {
+    type: 'START_LOCATION_FETCH'
+  }
+};
+
+var completeLocationFetch = (url) => {
+  return {
+    type: 'COMPLETE_LOCATION_FETCH',
+    url
+  }
+};
+
+var fetchLocation = () => {
+  store.dispatch(startLocationFetch());
+
+  axios.get('http://ipinfo.io').then(function (res) {
+    var loc = res.data.loc;
+    var baseUrl = 'http://maps.google.com?=';
+
+    store.dispatch(completeLocationFetch(baseUrl + loc));
+  });
+};
 var reducer = redux.combineReducers({
   name: nameReducer,
   hobbies: hobbiesReducer,
-  movies: moviesReducer
+  movies: moviesReducer,
+  map: mapReducer
 });
 
 var store = redux.createStore(reducer, redux.compose(
@@ -104,10 +148,16 @@ var unsubscribe = store.subscribe(() => {
   var state = store.getState();
 
   console.log('Name is: ', state.name);
-  document.getElementById('app').innerHTML = state.name;
+  if(state.map.isFetching) {
+    document.getElementById('app').innerHTML = 'Loading...';
+  } else if(state.map.url) {
+    document.getElementById('app').innerHTML = '<a href="' + state.map.url + '" target="_blank">View Your Location</a>'
+  }
   console.log('New state: ', store.getState());
 
 });
+
+fetchLocation();
 
 var currentState = store.getState();
 console.log('currentState', currentState);
@@ -123,7 +173,5 @@ store.dispatch(addMovie('La Fuente','Drama'));
 store.dispatch(addMovie('Star Wars', 'Action'));
 store.dispatch(addMovie('Ciudade de Deus', 'Drama'));
 store.dispatch(removeMovie(1));
-
-
 //console.log('Name should be Andrés', store.getState());
 store.dispatch(changeName('Mario'));
